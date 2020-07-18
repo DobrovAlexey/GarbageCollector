2ef2e6c45fa8a5d982fc0b8439f8a1901b2d228b
Алексей Добров <dobrov.alx@gmail.com>
2020-07-18 14:54:33 +0300

    Add Older-to-younger link tests

---
+++ GarbageCollectorFramework/OlderToYounger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GarbageCollectorFramework
+{
+    public class OlderToYounger
+    {
+        [TestMethod]
+        public void DoubleArrayInZeroGeneration()
+        {
+            var x = new Foo();  // x -> Gen0
+
+            Assert.AreEqual(GC.GetGeneration(x), 0);
+
+            GC.Collect();   // x -> Gen1
+
+            x.Field = new Boo();    // x.Field => Gen0
+
+            Assert.AreEqual(GC.GetGeneration(x), 1);    // x == Gen1
+            Assert.AreEqual(GC.GetGeneration(x.Field), 0);  // x.Field == Gen0
+        }
+
+        public class Foo
+        {
+            public Boo Field { get; set; }
+        }
+
+        public class Boo
+        {
+        }
+    }
+}
